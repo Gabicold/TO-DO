314a6a10c0a0ba7cf1d5cd2b1ebe8cde99fb7010
Gabicold <biel18102004@gmail.com>
2022-03-07 16:48:25 -0300

    TestKkK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Title = styled.h1 `
+    color: red;
+`
+
+
+export default class Todo extends Component{
+
+  state ={
+    tarefa:'',
+    lista: []
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      tarefa: event.target.value
+    })
+  }
+
+  add = (event) => {
+    let {lista, tarefa} = this.state
+    if(tarefa != ""){
+      this.setState({
+        tarefa: '',
+        lista:lista.concat({
+          tarefa:tarefa,
+          id: Date.now()
+        })
+      })
+    }
+    event.preventDefault()
+  }
+
+  remove = (id) =>{
+    let {lista} = this.state
+    this.setState({
+      lista: lista.filter((item)=> {
+        return item.id != id
+      })
+    })
+  }
+
+
+render(){
+  return(
+    <div>
+      <Title>ListaDeTarefas</Title>
+      <form>
+      <input onChange={this.handleChange} type="text" value={this.state.tarefa}/>
+      <button onClick={this.add}>Add</button>
+      </form>
+      
+      
+      <div>
+        {this.state.lista.map((item)=>(
+
+          <ul>
+            <li>{item.tarefa} 
+           
+            <img  onClick={() => {this.remove(item.id)}} 
+             src="https://cdn-icons-png.flaticon.com/512/126/126468.png" alt="lixeira" width="3%"></img>
+            </li>
+            
+          </ul>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
+}
